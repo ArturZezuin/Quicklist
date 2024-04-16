6235d73d73df18b3efa285261a3af298e9e81970
Artur Zeziulin <arturziaziulin@gmail.com>
2024-04-16 13:04:47 +0300

    rename app.py

--- app/__init__.py
+++ app/__init__.py
@@ -3,12 +3,14 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
+from flask_mail import Mail
 
 from config import Config
 
 db = SQLAlchemy()
 migrate = Migrate()
 login = LoginManager()
+mail = Mail()
 
 def create_app(config_class=Config):
     app = Flask(__name__)
@@ -17,6 +19,7 @@ def create_app(config_class=Config):
     db.init_app(app)
     migrate.init_app(app, db)
     login.init_app(app)
+    mail.init_app(app)
 
     from app.errors import bp as errors_bp
     app.register_blueprint(errors_bp)
