b26dc0648277810ecb9eae79e6b0313d0f6c2960
Artur Zeziulin <arturziaziulin@gmail.com>
2024-04-20 11:46:20 +0300

    reconstruction

--- app/static/js/edit.js
+++ app/static/js/edit.js
@@ -6,6 +6,8 @@ const listOffers = document.getElementById('listOffers');
 const listItemsWrapper = document.getElementById('listItemsWrapper');
 const listItems = document.getElementById('listItems');
 const messageToast = document.getElementById('messageToast');
+const blockSettings = document.getElementById('blockSettings');
+const btnSettings = document.getElementById('btnSettings');
 
 
 window.onload = function() {
@@ -52,8 +54,6 @@ function showHideListItems(){
                 }
             }
         }
-
-        document.getElementById('allItems').textContent = "All items: " + allItems;
         
         if (allItems == 0) {
             let div = document.createElement('div');
@@ -105,6 +105,10 @@ listItems.onclick = function(e) {
     }
 }
 
+btnSettings.onclick = function() {
+    blockSettings.style.display = (blockSettings.style.display == "none") ? "block" : "none";
+}
+
 textlistName.onchange = function() {
     let newName = textlistName.value;
     if (newName.length == 0 ) newName = 'New list'; 
@@ -139,10 +143,9 @@ function addItemToList(dbItemId, dbItemName){
                         <div class="px-0"><input class="form-check-input me-1" type="checkbox" value="" id="listCheckbox-` + dbItemId + `"></div>
                         <div class="flex-grow-1 px-2"><label role="button" class="form-check-label" for="listCheckbox-` + dbItemId + `">` + dbItemName + `</label></div>
                         <div role="button" class="deleteListItem">
-                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash" viewBox="0 0 16 16">
-                                <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5m2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5m3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0z"/>
-                                <path d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4zM2.5 3h11V2h-11z"/>
-                            </svg>   
+                            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" class="bi bi-x" viewBox="0 0 16 16">
+                                <path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708"/>
+                            </svg> 
                         </div>
                     </div>   
                     `;
@@ -173,7 +176,7 @@ function addItemToOfferList(textContent, dbItemID){
     div.textContent = textContent;
     div.setAttribute('dbItemID', dbItemID)
     div.setAttribute('role', "button")
-    div.className = "list-group-item list-group-item-action";
+    div.className = "list-group-item list-group-item-action bg-light px-2 py-1";
     listOffers.appendChild(div);  
     listOffersWrapper.style.display = "block";  
 }
